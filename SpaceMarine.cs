3840331ff8ac5b76a5c90fd3a97cb0f55037a8d9
lleclech <lleclech@enseirb-matmeca.fr>
2013-11-25 11:11:26 +0100

    purge du moveDemo, ajout des limites de terrain

--- SpaceMarine.cs
+++ SpaceMarine.cs
@@ -15,14 +15,94 @@ namespace Mogre.Tutorials
             ent.CastShadows = true;
             node = mSceneMgr.RootSceneNode.CreateChildSceneNode("RobotNode" + count.ToString());
             node.AttachObject(ent);
-            node.Scale(0.5f, 0.5f, 0.5f);
             name = count++.ToString();
             mWalkSpeed = 50.0f;
+            mAnimationState = ent.GetAnimationState("Idle");
+            mAnimationState.Loop = true;
+            mAnimationState.Enabled = true;
+            mWalkList = new LinkedList<Vector3>();
+            mWalkList.AddLast(new Vector3(550.0f, 0.0f, 50.0f));
+            mWalkList.AddLast(new Vector3(-100.0f, 0.0f, -200.0f));
+            mWalkList.AddLast(new Vector3(0.0f, 0.0f, 25.0f));
         }
         protected override void destroy() { }
-        protected override void move(FrameEvent evt)
+        protected bool nextLocation()
         {
-            
+            if (mWalkList.Count == 0)
+                return false;
+            return true;
         }
+        public override void move(FrameEvent evt, Environment env)
+        {
+            if (!mWalking)
+            //either we've not started walking or reached a way point
+            {
+                //check if there are places to go
+                if (nextLocation())
+                {
+                    LinkedListNode<Vector3> tmp;
+
+                    //Start the walk animation
+                    mAnimationState = ent.GetAnimationState("Walk");
+                    mAnimationState.Loop = true;
+                    mAnimationState.Enabled = true;
+                    mWalking = true;
+
+                    //Update the destination using the walklist.
+                    mDestination = mWalkList.First.Value; //get the next destination.
+                    tmp = mWalkList.First; //save the node that held it
+                    mWalkList.RemoveFirst(); //remove that node from the front of the list
+                    mWalkList.AddLast(tmp);  //add it to the back of the list.
+
+                    //update the direction and the distance
+                    mDirection = mDestination - node.Position;
+                    mDistance = mDirection.Normalise();
+
+                }//if(nextLocation())
+                else //nowhere to go. set the idle animation. (or Die)
+                {
+                    mAnimationState = ent.GetAnimationState("Idle");
+                    //mAnimationState = mEntity.GetAnimationState("Die");
+                    //mAnimationState.SetLoop(false);
+                }
+            }
+            else //we're in motion
+            {
+                //determine how far to move this frame
+                float move = mWalkSpeed * evt.timeSinceLastFrame;
+                mDistance -= move;
+                //Check to see if we've arrived at a waypoint
+                if (mDistance <= 0.0f || env.outOfGround(node.Position))
+                {
+                    //set our node to the destination we've just reached & reset direction to 0
+                    node.Position=mDestination;
+                    mDirection = Vector3.ZERO;
+                    mWalking = false;
+                }//if(mDistance <= 0.0f)
+                else
+                {
+                    //Rotation code goes here
+                    Vector3 src = node.Orientation * Vector3.UNIT_X;
+                    if ((1.0f + src.DotProduct(mDirection)) < 0.0001f)
+                    {
+                        node.Yaw(180.0f);
+                    }
+                    else
+                    {
+                        Quaternion quat = src.GetRotationTo(mDirection);
+                        node.Rotate(quat);
+                    }
+                    //movement code goes here
+                    node.Translate(mDirection * move);
+                }
+
+            }
+            //Update the Animation State.
+            mAnimationState.AddTime(evt.timeSinceLastFrame * mWalkSpeed / 20);
+
+        }
+        
+
+
     }
 }
--- Tutorial.cs
+++ Tutorial.cs
@@ -9,107 +9,6 @@ using System.Text;
 
 namespace Mogre.Tutorials
 {
-    /*class Tutorial : BaseApplication
-    {
-        public static void Main()
-        {
-            new Tutorial().Go();
-        }
-
-        protected override void CreateScene()
-        {
-            //shadow
-            mSceneMgr.AmbientLight = ColourValue.Black;
-            mSceneMgr.ShadowTechnique = ShadowTechnique.SHADOWTYPE_STENCIL_ADDITIVE;
-            
-            // Pitch is rotation around the x axis, 
-            // yaw is around the y axis
-            // roll is around the z axis.
-            
-            //mSceneMgr.AmbientLight = new ColourValue(1, 1, 1);
-            ///Entity ent = mSceneMgr.CreateEntity("Head", "ninja.mesh");
-            //ent.CastShadows = true;
-            //SceneNode node = mSceneMgr.RootSceneNode.CreateChildSceneNode("HeadNode");
-            //node.AttachObject(ent);
-            //node.Scale(0.5f,0.5f, 0.5f);
-            //node.Yaw(new Degree(-45));
-            //node.Pitch(new Degree(-45));
-            //node.Roll(new Degree(45));
-
-            Ninja n= new Ninja(mSceneMgr,Vector3.ZERO);
-            
-            //Entity ent3 = mSceneMgr.CreateEntity("Head3", "ninja.mesh");
-            //ent3.CastShadows = true;
-            //SceneNode node3 = mSceneMgr.RootSceneNode.CreateChildSceneNode("HeadNode3", new Vector3(100, 00, 0));
-            //node3.AttachObject(ent3);
-            //node3.Scale(0.5f, 0.5f, 0.5f);
-            
-            //node2.Position += new Vector3(10, 0, 10);
-
-            //create ground
-            Plane plane = new Plane(Vector3.UNIT_Y, 0);
-
-            MeshManager.Singleton.CreatePlane("ground",
-                ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME, plane,
-                1500, 1500, 20, 20, true, 1, 5, 5, Vector3.UNIT_Z);
-
-            Entity groundEnt = mSceneMgr.CreateEntity("GroundEntity", "ground");
-            mSceneMgr.RootSceneNode.CreateChildSceneNode().AttachObject(groundEnt);
-
-            groundEnt.SetMaterialName("Examples/Rockwall");
-            groundEnt.CastShadows = false;
-
-            //ligth
-            Light pointLight = mSceneMgr.CreateLight("pointLight");
-            pointLight.Type = Light.LightTypes.LT_POINT;
-            pointLight.Position = new Vector3(0, 150, 250);
-            pointLight.DiffuseColour = ColourValue.Red;
-            pointLight.SpecularColour = ColourValue.Red;
-
-            Light directionalLight = mSceneMgr.CreateLight("directionalLight");
-            directionalLight.Type = Light.LightTypes.LT_DIRECTIONAL;
-            directionalLight.DiffuseColour = new ColourValue(.25f, .25f, 0);
-            directionalLight.SpecularColour = new ColourValue(.25f, .25f, 0);
-            directionalLight.Direction = new Vector3(0, -1, 1);
-
-            Light spotLight = mSceneMgr.CreateLight("spotLight");
-            spotLight.Type = Light.LightTypes.LT_SPOTLIGHT;
-            spotLight.DiffuseColour = ColourValue.Blue;
-            spotLight.SpecularColour = ColourValue.Blue;
-            spotLight.Direction = new Vector3(-1, -1, 0);
-            spotLight.Position = new Vector3(300, 300, 0);
-
-            spotLight.SetSpotlightRange(new Degree(35), new Degree(50));
-        }
-
-        protected override void CreateCamera()
-        {
-            mCamera    = mSceneMgr.CreateCamera("PlayerCam");
-            mCameraMan = new CameraMan(mCamera);
-            mCamera.Position = new Vector3(0, 10, 500);
-            mCamera.LookAt(Vector3.ZERO);
-            //mCamera.Yaw(new Degree(45));
-            mCamera.NearClipDistance = 100;
-            mCamera.FarClipDistance = 500;
-            mCameraMan = new CameraMan(mCamera);
-
-            
-
-        }
-        protected override void CreateViewports()
-        {
-            Viewport viewport = mWindow.AddViewport(mCamera);
-            viewport.BackgroundColour = ColourValue.Blue;
-            mCamera.AspectRatio = (float)viewport.ActualWidth / viewport.ActualHeight;
-        }
-        public void createNinja()
-        {
-            Entity ent2 = mSceneMgr.CreateEntity("Head2", "robot.mesh");
-            ent2.CastShadows = true;
-            SceneNode node2 = mSceneMgr.RootSceneNode.CreateChildSceneNode("HeadNode2", new Vector3(0, 0, 100));
-            node2.AttachObject(ent2);
-        }
-    }*/
     public class Tutorial
     {
         protected static Root mRoot;
@@ -186,24 +85,6 @@ namespace Mogre.Tutorials
             ResourceGroupManager.Singleton.InitialiseAllResourceGroups();
         }
 
-        /* tuto create scene
-        protected static void CreateScene()
-        {
-            SceneManager sceneMgr = mRoot.CreateSceneManager(SceneType.ST_GENERIC);
-            Camera camera = sceneMgr.CreateCamera("Camera");
-            camera.Position = new Vector3(0, 0, 150);
-            camera.LookAt(Vector3.ZERO);
-            mRenderWindow.AddViewport(camera);
-
-            Entity ogreHead = sceneMgr.CreateEntity("Head", "ogrehead.mesh");
-            SceneNode headNode = sceneMgr.RootSceneNode.CreateChildSceneNode();
-            headNode.AttachObject(ogreHead);
-
-            sceneMgr.AmbientLight = new ColourValue(0.5f, 0.5f, 0.5f);
-
-            Light l = sceneMgr.CreateLight("MainLight");
-            l.Position = new Vector3(20, 80, 50);
-        }*/
 
         protected virtual void CreateScene()
         {
@@ -211,67 +92,7 @@ namespace Mogre.Tutorials
 
             Environment env = new Environment(ref mSceneMgr);
             
-            //mSceneMgr.AmbientLight = ColourValue.Black;
-            //mSceneMgr.ShadowTechnique = ShadowTechnique.SHADOWTYPE_STENCIL_ADDITIVE;
-
-            //// Pitch is rotation around the x axis, 
-            //// yaw is around the y axis
-            //// roll is around the z axis.
-
-            ////mSceneMgr.AmbientLight = new ColourValue(1, 1, 1);
-            /////Entity ent = mSceneMgr.CreateEntity("Head", "ninja.mesh");
-            ////ent.CastShadows = true;
-            ////SceneNode node = mSceneMgr.RootSceneNode.CreateChildSceneNode("HeadNode");
-            ////node.AttachObject(ent);
-            ////node.Scale(0.5f,0.5f, 0.5f);
-            ////node.Yaw(new Degree(-45));
-            ////node.Pitch(new Degree(-45));
-            ////node.Roll(new Degree(45));
-
-            //Ninja n = new Ninja(ref mSceneMgr, Vector3.ZERO);
-
-            ////Entity ent3 = mSceneMgr.CreateEntity("Head3", "ninja.mesh");
-            ////ent3.CastShadows = true;
-            ////SceneNode node3 = mSceneMgr.RootSceneNode.CreateChildSceneNode("HeadNode3", new Vector3(100, 00, 0));
-            ////node3.AttachObject(ent3);
-            ////node3.Scale(0.5f, 0.5f, 0.5f);
-
-            ////node2.Position += new Vector3(10, 0, 10);
-
-            ////create ground
-            //Plane plane = new Plane(Vector3.UNIT_Y, 0);
-
-            //MeshManager.Singleton.CreatePlane("ground",
-            //    ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME, plane,
-            //    1500, 1500, 20, 20, true, 1, 5, 5, Vector3.UNIT_Z);
-
-            //Entity groundEnt = mSceneMgr.CreateEntity("GroundEntity", "ground");
-            //mSceneMgr.RootSceneNode.CreateChildSceneNode().AttachObject(groundEnt);
-
-            //groundEnt.SetMaterialName("Examples/Rockwall");
-            //groundEnt.CastShadows = false;
-
-            ////ligth
-            //Light pointLight = mSceneMgr.CreateLight("pointLight");
-            //pointLight.Type = Light.LightTypes.LT_POINT;
-            //pointLight.Position = new Vector3(0, 150, 250);
-            //pointLight.DiffuseColour = ColourValue.Red;
-            //pointLight.SpecularColour = ColourValue.Red;
-
-            //Light directionalLight = mSceneMgr.CreateLight("directionalLight");
-            //directionalLight.Type = Light.LightTypes.LT_DIRECTIONAL;
-            //directionalLight.DiffuseColour = new ColourValue(.25f, .25f, 0);
-            //directionalLight.SpecularColour = new ColourValue(.25f, .25f, 0);
-            //directionalLight.Direction = new Vector3(0, -1, 1);
-
-            //Light spotLight = mSceneMgr.CreateLight("spotLight");
-            //spotLight.Type = Light.LightTypes.LT_SPOTLIGHT;
-            //spotLight.DiffuseColour = ColourValue.Blue;
-            //spotLight.SpecularColour = ColourValue.Blue;
-            //spotLight.Direction = new Vector3(-1, -1, 0);
-            //spotLight.Position = new Vector3(300, 300, 0);
-
-            //spotLight.SetSpotlightRange(new Degree(35), new Degree(50));
+            
         }
 
         protected void CreateCamera()
